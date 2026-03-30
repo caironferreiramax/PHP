6c7a0d48cdf31fda4afc9ab0a9ce5d937496fcef
caironferreiramax <caironferreira01@gmail.com>
2026-03-30 20:21:49 -0300

    aprendendo PHP

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+echo "Hellow World!";
+
+$nome = "Cairon";
+$idade = 25;
+$_1idade = 30;
+
+$soma = 2 + 2;
+$subtracao = 3 - 1;
+$multiplicao = 2 * 2;
+$divisao = 4 / 2;
+$modulacao = 4 % 2;
+$exponente = 4 ** 2;
+
+$somaPrecedencia = (1+2) * 2;
+
+$valorFloat = 4.8200;
+$arr = [10, 20, 30];
+$mat = [[10,10], [20,20]];
+
+echo "Olá Mundo";
+echo 'Olá Mundo';
+
+$helloworld = "Hellow World";
+
+echo $HellowWorld;
+
+// Concatenação
+echo ` Hoje` . ` Está` . ` um lindo dia<br>`;
+// Interpolação
+$exemplo - "Lindo dia<br>";
+echo "Hoje esta um $exemplo";
